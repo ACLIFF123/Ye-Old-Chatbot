f0aa2f1857a445ac214a3bad5353673de428c2be
ACLIFF123 <aaronclifford26@gmail.com>
2025-11-21 13:36:45 +0000

    feat: added generate reply function and spoke to chat bot

--- src/chatbot.py
+++ src/chatbot.py
@@ -9,8 +9,27 @@ class Chatbot:
     def encode_prompt(self, prompt: str):
         return self.tokenizer(prompt, return_tensors="pt")
     
+    def decode_reply(self, reply_ids: list[int]) -> str:
+        return self.tokenizer.decode(reply_ids, skip_special_token=True)
     
-    
-    
+    def generate_reply(self, prompt: str) -> str:
+        prompt_with_new_line = prompt + "\n"
+
+        inputs = self.encode_prompt(prompt_with_new_line)
+
+        output = self.model.generate(**inputs, pad_token_id=self.tokenizer.eos_token_id, do_sample=True, 
+        temperature=0.8, # Higher = more randomness (range: ~0.7 to 1.2)
+        top_p=0.5, # Nucleus sampling: picks from top tokens with cumulative probability <= top_p 
+        top_k=20)[0] # Only consider the top 50 most likely tokens 
+
+        decode = self.decode_reply(output)
+
+        reply = decode[len(prompt_with_new_line):]
+
+        return reply 
+
+
+
+
     
      
--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from chatbot import Chatbot
 
 bot = Chatbot()
-encoded = bot.encode_prompt("Hello, how are you?")
-print(encoded)
+# encoded = bot.encode_prompt("Hello, how are you?")
+# reply = bot.decode_reply([15496, 703, 345, 30]) # Pass in a string of generated token IDs here from your tokenizer
+
+# print(encoded) 
+# print(reply)
 
 
-  
-
+prompt = "What is the weather like today?"
+reply = bot.generate_reply(prompt)
+print(f"Prompt: {prompt}")
+print(f"Reply: {reply}")
