16d83d30d60b295201c7cc340a57a515c2c42f24
ACLIFF123 <aaronclifford26@gmail.com>
2025-11-21 14:57:17 +0000

    fear:add conversation history and reset_history

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,10 +1,13 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch 
+
 
 class Chatbot:
     def __init__(self):
         self.model_name = "microsoft/DialoGPT-small"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.model = AutoModelForCausalLM.from_pretrained(self.model_name)
+        self.chat_history_ids = None
 
     def encode_prompt(self, prompt: str):
         return self.tokenizer(prompt, return_tensors="pt")
@@ -15,20 +18,32 @@ class Chatbot:
     def generate_reply(self, prompt: str) -> str:
         prompt_with_new_line = prompt + "\n"
 
-        inputs = self.encode_prompt(prompt_with_new_line)
+        encoded = self.encode_prompt(prompt_with_new_line)
 
-        output = self.model.generate(**inputs, pad_token_id=self.tokenizer.eos_token_id, do_sample=True, 
-        temperature=0.8, # Higher = more randomness (range: ~0.7 to 1.2)
-        top_p=0.5, # Nucleus sampling: picks from top tokens with cumulative probability <= top_p 
-        top_k=20)[0] # Only consider the top 50 most likely tokens 
-
-        decode = self.decode_reply(output)
-
-        reply = decode[len(prompt_with_new_line):]
-
-        return reply 
+        new_input = encoded["input_ids"]
 
 
+        if self.chat_history_ids is None:
+            input_ids = new_input
+        else:
+            input_ids = torch.cat([self.chat_history_ids, new_input], dim=-1)
+        
+        output = self.model.generate(input_ids, pad_token_id=self.tokenizer.eos_token_id, do_sample=True, 
+        temperature=0.9, # Higher = more randomness (range: ~0.7 to 1.2)
+        top_p=0.8, # Nucleus sampling: picks from top tokens with cumulative probability <= top_p 
+        top_k=50) # Only consider the top 50 most likely tokens 
+
+        self.chat_history_ids = output
+        reply_id = output[0, input_ids.shape[1]:]
+        reply = self.decode_reply(reply_id.tolist()).strip()
+       
+        return reply
+    
+    def reset_history(self):
+        self.chat_history_ids = None 
+        
+
+    
 
 
     
--- src/main.py
+++ src/main.py
@@ -8,7 +8,18 @@ bot = Chatbot()
 # print(reply)
 
 
-prompt = "What is the weather like today?"
-reply = bot.generate_reply(prompt)
-print(f"Prompt: {prompt}")
-print(f"Reply: {reply}")
+# prompt = "What is the weather like today?"
+# reply = bot.generate_reply(prompt)
+# print(f"Prompt: {prompt}")
+# print(f"Reply: {reply}")
+
+prompts = [
+    "What's your name?",
+    "What do you think about AI?",
+    "Sorry, tell me your name again."
+]
+
+for prompt in prompts:
+    reply = bot.generate_reply(prompt)
+    print(f"Prompt: {prompt}")
+    print(f"Reply: {reply}\n")
